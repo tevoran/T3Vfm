846dc2ac40cfaefca77135bc74c54c9d28e12e9d
tevoran <j.pfeiffer93@gmx.de>
2021-07-15 19:23:25 +0200

    application now gets a connection to X

--- src/main.c
+++ src/main.c
@@ -1,5 +1,32 @@
-#include <stdio.h>
+#include "t3vfm.h"
+
 void main()
 {
-	printf("hello world\n");
+	printf("starting T3Vfm\n");
+
+	//initialization of X
+	Display *display;
+	Window window;
+	XEvent event;
+
+	printf("init X...");
+	display = XOpenDisplay(NULL);
+	if(display!=NULL)
+	{
+		printf("done\n");
+	}
+	else
+	{
+		printf("error\n");
+		return;
+	}
+
+
+	//cleaning up
+	printf("ending connection to X...");
+	XCloseDisplay(display);
+	printf("done\n");
+
+	printf("ending T3Vfm\n");
+	return;
 }
---
+++ src/t3vfm.h
@@ -0,0 +1,8 @@
+#ifndef T3VFM
+#define T3VFM
+
+#include <X11/Xlib.h>
+#include <stdio.h>
+
+
+#endif
